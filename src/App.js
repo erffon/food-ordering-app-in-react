4f56ddf8681491abd073a6c88fafad38e17890a1
office <nexus1377@gmail.com>
2022-08-15 13:59:16 +0430

    some bug fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from "./components/layout/Header";
 import {Fragment} from "react";
+import Meals from './components/meals/Meals';
 
 
 function App() {
   return (
     <Fragment>
       <Header/>
+      <main>
+        <Meals/>
+      </main>
     </Fragment>
   );
 } 
---
+++ src/components/meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+import { Fragment } from "react";
+
+const Meals = ()=>{
+    return (
+        <Fragment>
+            <MealsSummary/>
+            <AvailableMeals/>
+        </Fragment>
+    );
+}
+export default Meals;
