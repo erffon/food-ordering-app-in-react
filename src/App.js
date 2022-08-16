ab9ad159a51befa96af6bb04bd841f75451e4a5e
office <nexus1377@gmail.com>
2022-08-16 22:19:35 +0430

    some functionality for modal has been added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import Header from "./components/layout/Header";
 import {Fragment} from "react";
 import Meals from './components/meals/Meals';
 import Cart from "./components/Cart/Cart";
+import {useState} from "react";
 
 
 
 function App() {
+
+  const [modalIsShowed,setModalIsShowed]=useState(false);
+
+  const modalIsShowHandler = ()=>{
+    setModalIsShowed(true);
+  }
+  const modalNotShowed = ()=>{
+    setModalIsShowed(false);
+  }
+
   return (
     <Fragment>
-      <Cart/>
+      {modalIsShowed && <Cart/>}
       <Header/>
       <main>
         <Meals/>
